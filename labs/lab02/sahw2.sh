f5b1ce1bc4d5f4501b7d99ec25f9c0ad56109d2f
Chun <roger20020122@gmail.com>
2023-03-18 12:30:54 +0800

    usage

--- labs/lab02/sahw2.sh
+++ labs/lab02/sahw2.sh
@@ -2,8 +2,7 @@
 
 # usage
 usage() {
-    usage() {
-        echo -n -e "\nUsage: sahw2.sh {--sha256 hashes ... | --md5 hashes ...} -i files ...\n\n--sha256: SHA256 hashes to validate input files.\n--md5: MD5 hashes to validate input files.\n-i: Input files.\n"
+    echo -n -e "\nUsage: sahw2.sh {--sha256 hashes ... | --md5 hashes ...} -i files ...\n\n--sha256: SHA256 hashes to validate input files.\n--md5: MD5 hashes to validate input files.\n-i: Input files.\n"
 }
 
 }
